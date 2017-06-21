96fa1f156a552a5da0b20c1636c62ff33e02fac5
haifahooriyah <haifa.hooriyah@gmail.com>
2017-06-22 04:52:48 +0530

    Adding DB connector project

---
+++ src/main/java/com/liquidhub/db/Database.java
@@ -0,0 +1,68 @@
+package com.liquidhub.db;
+
+/**
+ * Created by hasham on 22-06-2017.
+ */
+public class Database {
+    private String host;
+    private String port;
+    private String database;
+    private String user;
+    private String password;
+
+    /**
+     * @param host - The database hostname.
+     * @param port - The database port.
+     * @param database - The database name.
+     * @param user - The database username.
+     * @param password the database password.
+     */
+    public Database(String host,String port,String database,String user,String password) {
+        this.host =host;
+        this.port = port;
+        this.database =database;
+        this.user = user;
+        this.password = password;
+    }
+
+
+    /**
+     * @return the host
+     */
+    public String getHost() {
+        return host;
+    }
+
+
+    /**
+     * @return the port
+     */
+    public String getPort() {
+        return port;
+    }
+
+
+    /**
+     * @return the database
+     */
+    public String getDatabase() {
+        return database;
+    }
+
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+}
+
---
+++ src/main/java/com/liquidhub/db/model/connectors/DataAccessor.java
@@ -0,0 +1,45 @@
+package com.liquidhub.db.model.connectors;
+
+import com.liquidhub.db.Database;
+
+import java.sql.SQLException;
+
+/**
+ * Created by hasham on 22-06-2017.
+ */
+public interface DataAccessor {
+    /**
+     * Register JDBC driver and OPEN a connection.
+     * @param database
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public void initialize(Database database) throws ClassNotFoundException, SQLException;
+
+    /**
+     *
+     * @param host - The database hostname.
+     * @param port - The database port.
+     * @param database - The database name.
+     * @return the database URL.
+     */
+    public String getConnectionString(String host, String port, String database);
+
+    /**
+     * Register JDBC driver and CLOSE a connection.
+     * @param database
+     * @throws SQLException
+     */
+    public void closeConnection(Database database) throws SQLException;
+
+
+    /**
+     *  Execute Insert query and returns
+     *  @param query
+     *  @throws SQLException
+     */
+    public boolean executeInsertQuery(String query) throws SQLException;
+
+    
+    
+}
